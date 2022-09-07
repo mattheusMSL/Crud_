a9af5cee2c6654540c0b0465d02fff158e1a81af
kelvinkauan <kelvinkauan9@gmail.com>
2022-09-07 17:43:15 -0300

    first commit

---
+++ controller/Usuariocontroller.php
@@ -0,0 +1,129 @@
+<?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors',1);
+    error_reporting(E_ALL);
+
+    require_once __DIR__."/../repository/Usuariorepository.php";
+
+    $usuario = new Usuariocontroller();
+    
+    class Usuariocontroller{
+        function __construct(){
+            if(isset($_POST["action"])){
+                $action = $_POST["action"];
+            }elseif(isset($_GET["action"])){
+                $action=$_GET["action"];
+            }
+            if(isset($action)){
+                $this->callAction($action);
+            }else{
+                $msg="NENHUMA ACAO ENCONTRADA";
+                print_r($msg);
+
+            }
+            
+        }
+
+        public function callAction(String $functionName){
+            if(method_exists($this,$functionName)){
+                $this->$functionName;
+            }else if(method_exists($this,"preventDefault")){
+                $met = "preventDefault";
+                $this->$met;
+            }else{
+                throw new BadFunctionCallException("usecase not exists");
+            }
+        }
+
+        public function loadView(string $path, array $data =null, string $msg = null){
+        $caminho= __DIR__. "/../views/".$path;
+        if(file_exists($caminho)){
+            require $caminho;
+        }else{
+            print("Erro ao carregar views");
+        }
+    }
+
+    private function criar(){
+        $usuario = new Usuariomodels();
+        $usuario->setNome($_POST["nome"]);
+        $usuario->setEmail($_POST["email"]);
+        $usuario->setSenha($_POST["senha"]);
+
+        $Usuariorepository = new UsuarioRepository();
+
+        $id = $Usuariorepository->criar($usuario);
+
+        if($id){
+            $msg ="REGISTRO INSERIDO COM SUCESSO!";
+        }else{
+            $msg="ERRO AO INSERIR O REGISTRO NO BANCO DE DADOS";
+        }
+        $this->findAll();
+    }
+    private function loadForm(){
+        $this->loadView("views/Usuarios/Usuariocadastro.php", null, "teste");
+    }
+
+    private function findAll(string $msg = null){
+        $Usuariorepository= new UsuarioRepository();
+        $usuario = $Usuariorepository->findAll();
+
+        $data['titulo']= "listar Usuarios";
+        $data['usuarios'] = $usuario; 
+
+        $this-> loadView("Usuarios/Usuariolista.php",$data,$msg);
+
+    }
+    private function FindUsuarioById(){
+        $idParam = $_GET('id');
+        $Usuariorepository = new UsuarioRepository();
+        $usuario = $Usuariorepository->findUsuarioById($idParam);
+        print "<prev>";
+        print ($usuario);
+        print"prev";
+    }
+
+    private function DeletarUsuarioById(){
+        $idParam = $_GET('id');
+        $Usuariorepository = new UsuarioRepository();
+        $dt= $Usuariorepository -> DeleteById($idParam);
+        if($dt){
+            $msg = "REGISTRO EXCLUÍDO COM SUCESSO";
+        }else{
+            $msg = "FALHA AO EXCLUIX REGISTRO!";
+        }
+        $this->findAll($msg);
+    }
+     
+    private function editarUsuario(){
+        $idParam = $_GET('id');
+        $Usuariorepository = new UsuarioRepository();
+        $usuario= $Usuariorepository-> findUsuarioById($idParam);
+        $data['usuario'][0] = $usuario;
+
+        $this->loadView("Usuarios/Usuariolista.php", $data);
+    }
+
+    private function atualizarUsuario(){
+        $usuario = new Usuariomodels();
+        $usuario -> setId($_GET["id"]);
+        $usuario-> setNome($_POST["nome"]);
+        $usuario->setEmail($_POST["email"]);
+        $usuario-> setSenha($_POST["senha"]);
+
+        $Usuariorepository = new UsuarioRepository();
+        $atualizar = $Usuariorepository->atualizar($usuario);
+        if($atualizar){
+            $msg= "REGISTRO ATUALIZADO COM SUCESSO!";
+        }else{
+            $msg = "ERRO AO ATUALIZAR REGISTRO!";
+        }
+        $this->findAll($msg);
+    }
+private function preventDefault(){
+    print "AÇÃO INDEFINIDA...";
+   }   
+
+    
+}
---
+++ repository/Usuariorepository.php
@@ -0,0 +1,76 @@
+<?php
+
+
+
+require_once __DIR__."/../connection/Usuarioconnection.php";
+require_once __DIR__."/../models/Usuariomodels.php";
+
+class UsuarioRepository{
+    public PDO $conn;
+
+    function __construct()
+    {
+       $this-> conn = Connection::getConnection();
+    }
+
+    public function criar (Usuariomodels $usuario) : int {
+        try{
+            $query ="INSERT INTO usuario (nome, email, senha) VALUES (:nome, :email, :senha)";
+            $prepare= $this->conn->prepare($query);
+            $prepare->bindValue(":nome", $usuario->getNome());
+            $prepare->bindValue(":email", $usuario->getEmail());
+            $prepare->bindValue(":senha", $usuario->getSenha());
+            $prepare->execute();
+            return $this->conn->lastInsertId();
+
+        }catch(Exception $e){
+            print("ERRO AO INSERIR USUARIO NO BANCO DE DADOS!");          
+        }
+    }
+
+    public function findAll(): array{
+        $tabela = $this->conn->query("SELECT*FROM usuarios");
+        $usuario = $tabela->fetchAll(PDO::FETCH_ASSOC);
+        return $usuario; 
+    }
+    
+    public function findUsuarioById(int $id){
+        $query = "SELECT*FROM usuarios WHERE id = ?";
+        $prepare = $this->conn->prepare($query);
+        $prepare->bindParam(1, $id, PDO::PARAM_INT);
+
+        if($prepare->execute()){
+            $usuario = $prepare->fetchObject("Usuariomodels");
+        }else{
+            $usuario=null;
+        }
+        return $usuario;
+    }
+
+    public function atualizar(Usuariomodels $usuario) : bool {
+        $query = "UPDATE usuarios SET nome = ?, email = ?, senha = ? WHERE id = ?";
+        $prepare = $this->conn->prepare($query);
+        $prepare->bindValue(1, $usuario->getNome());
+        $prepare->bindValue(2, $usuario->getEmail());
+        $prepare->bindValue(3, $usuario->getSenha());
+        $prepare->bindValue(4, $usuario -> getId());
+        $result= $prepare->execute();
+    return $result;                
+
+    }
+
+    public function DeleteById(int $id):int{
+        $query = "DELETE FROM usuarios WHERE id = :id";
+        $prepare = $this->conn->prepare($query);
+        $prepare->bindValue(":id", $id);
+        $prepare->execute();
+        $result=$prepare->rowCount();
+        return $result;
+
+
+    }
+
+
+
+
+}

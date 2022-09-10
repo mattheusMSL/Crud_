6ef90697439d79ba3fc6347ec28641c302cd6252
kelvinkauan <kelvinkauan9@gmail.com>
2022-09-10 16:12:04 -0300

    Changes to be committed: 	modified:   BKM_DB.sql

--- BKM_DB.sql
+++ BKM_DB.sql
@@ -1,15 +1,11 @@
 
 use projeto_integrador;
 
-#create table usuarios(
-#idUsuario int not null auto_increment,
-#nc_Usuario varchar(60),
-#email_Usuario varchar(50),
-#senha_Usuario varchar(12),
-#PRIMARY KEY (idUsuario)
-#);
-show tables;
+create table usuarios(
+idUsuario int not null auto_increment,
+nc_Usuario varchar(60),
+email_Usuario varchar(50),
+senha_Usuario varchar(12),
+PRIMARY KEY (idUsuario)
+);
 
-desc usuarios;
-
-select*from tbusuarios;

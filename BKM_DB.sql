0f2e0f9dea883a1285298c3efa7e94ef8a5c0fee
kelvinkauan <kelvinkauan9@gmail.com>
2022-09-10 16:43:26 -0300

    vai que vai

--- BKM_DB.sql
+++ BKM_DB.sql
@@ -1,11 +1,16 @@
 
 use projeto_integrador;
-
+drop table if exists usuarios;
 create table usuarios(
-idUsuario int not null auto_increment,
+id int not null auto_increment,
 nc_Usuario varchar(60),
-email_Usuario varchar(50),
-senha_Usuario varchar(12),
-PRIMARY KEY (idUsuario)
-);
+email varchar(50),
+senha varchar (12),
+primary key(id)
+)engine=InnoDB auto_increment=5 default charset=utf8mb3;
 
+lock table usuarios write;
+insert into usuarios values
+(1,'kelvin','kelvin@silva.com','111'),(2,'alvin','alvi@gomes.com','2222');
+unlock tables;
+select*from usuarios;
